f600778f8410d3694446e5431730a2b5c77ed30a
mu-777 <ryosuke.murata.7@gmail.com>
2017-02-26 03:52:28 +0900

    support group and scene

--- src/ros_hue_controller.py
+++ src/ros_hue_controller.py
@@ -4,8 +4,13 @@
 # ref:
 #   https://github.com/studioimaginaire/phue
 import sys
+from os import path
+import json
+
+sys.path.append(path.abspath(path.dirname(__file__)) + "/../lib")
+
 import rospy
-from ros_hue.srv import IDsTrigger, IDsTriggerResponse
+from ros_hue.srv import HueTrigger, HueTriggerResponse
 from std_srvs.srv import Trigger, TriggerResponse
 
 try:
@@ -47,33 +52,66 @@ class ROSHueController(object):
     def activate(self):
         self.is_activated = True
         rospy.Service(rospy.get_param(SERVICE_NAME_INITIALIZE), Trigger, self.discover)
-        rospy.Service(rospy.get_param(SERVICE_NAME_ON), IDsTrigger, self.switch(True))
-        rospy.Service(rospy.get_param(SERVICE_NAME_OFF), IDsTrigger, self.switch(False))
-        rospy.Service(rospy.get_param(SERVICE_NAME_TOGGLE), IDsTrigger, self.switch(None))
+        rospy.Service(rospy.get_param(SERVICE_NAME_ON), HueTrigger, self.turn_on)
+        rospy.Service(rospy.get_param(SERVICE_NAME_OFF), HueTrigger, self.turn_off)
+        rospy.Service(rospy.get_param(SERVICE_NAME_TOGGLE), HueTrigger, self.toggle)
         return self
 
     def discover(self, req):
         self._bridge.connect()
         return TriggerResponse(success=True, message="")
 
-    def switch(self, val):
-        def _switch(req):
-            req.ids = self._light_ids if len(req.ids) == 0 else req.ids
-            rospy.loginfo("Turn " + ("on" if val else "off"))
-            self._bridge.set_light(req.ids, "on", value=val, transitiontime=0)
-            return IDsTriggerResponse(success=True, message="")
+    def turn_on(self, req):
+        rospy.logdebug("Turn on")
 
-        def _toggle(req):
-            val = not self.is_on()
-            req.ids = self._light_ids if len(req.ids) == 0 else req.ids
-            rospy.loginfo("Turn " + ("on" if val else "off"))
-            self._bridge.set_light(req.ids, "on", value=val, transitiontime=0)
-            return IDsTriggerResponse(success=True, message="")
+        group_ids = [g.group_id for g in self._bridge.groups if g.name.count(req.group_name)]
+        scene_ids = [s.scene_id for s in self._bridge.scenes if s.name.count(req.scene_name)]
+        if req.is_scene:
+            g_id = group_ids[0] if req.is_group and len(group_ids) > 0 else 0
+            if len(scene_ids) < 1:
+                return HueTriggerResponse(success=False,
+                                          message="Not found the scene: " + req.scene_name)
+            self._bridge.activate_scene(g_id, scene_ids[0])
+            return HueTriggerResponse(success=True, message="")
 
-        return _switch if val is not None else _toggle
+        param = {"on": True}
+        if req.is_hsv:
+            param.update({"hue": max(req.hue, 65534)}) if req.hue >= 0 else param.update({"hue_inc": 0})
+            param.update({"sat": max(req.sat, 254)}) if req.sat >= 0 else param.update({"sat_inc": 0})
+            param.update({"bri": max(req.bri, 254)}) if req.bri >= 0 else param.update({"bri_inc": 0})
+
+        if req.is_group:
+            if len(group_ids) == 0:
+                return HueTriggerResponse(success=False,
+                                          message="Not found the group: " + req.group_name)
+            self._bridge.set_group(group_ids, param, transitiontime=0)
+        else:
+            ids = self._light_ids if len(req.ids) == 0 else req.ids
+            self._bridge.set_light(ids, param, transitiontime=0)
+
+        return HueTriggerResponse(success=True, message="")
+
+    def turn_off(self, req):
+        param = {"on": False}
+        if req.is_group:
+            group_ids = [g.group_id for g in self._bridge.groups if g.name.count(req.group_name)]
+            if len(group_ids) == 0:
+                return HueTriggerResponse(success=False,
+                                          message="Not found the group: " + req.group_name)
+            self._bridge.set_group(group_ids, param, transitiontime=0)
+        else:
+            ids = self._light_ids if len(req.ids) == 0 else req.ids
+            self._bridge.set_light(ids, param, transitiontime=0)
+
+        return HueTriggerResponse(success=True, message="")
+
+    def toggle(self, req):
+        val = not self.is_on()
+        return self.turn_on(req) if val else self.turn_off(req)
 
     def is_on(self):
-        return True
+        states = self._bridge.get_light()
+        return all([states[str(id)]["state"]["on"] for id in self._light_ids])
 
 
 # --------------------------------------------
